ec14e0a9dfe310ea87538dc12fd5299623a0b78d
Markku Järvinen <62094844+Markeesi@users.noreply.github.com>
2023-08-30 12:24:04 +0300

    tehtävä tehty

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("button");
+  const fontKoko = document.getElementById("fontkoko");
+  const fontKokoNyt = document.getElementById("nyt");
+
+  let fontinKoko = 16;
+  fontKoko.style.fontSize = fontinKoko + "px"; // set the fontsize to 16px
+
+  button.onclick = () => {
+    fontinKoko += 4;
+    if (fontinKoko > 32) {
+      fontinKoko = 16;
+    }
+
+    fontKoko.style.fontSize = fontinKoko + "px"; // Update the font size after the change
+  };
+});
